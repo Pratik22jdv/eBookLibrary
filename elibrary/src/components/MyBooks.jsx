2a472872c335a3d30c906a10407e2dc826f859d6
adarsh27april <adarshkumar27april@gmail.com>
2021-10-24 09:54:51 +0530

    aks-2: trying to fix displaying of borrowed books
    final return statement ke andar console.log krne pr bookData print kar raha hai lekin html me display nahi ho rha hai

--- elibrary/src/components/MyBooks.jsx
+++ elibrary/src/components/MyBooks.jsx
@@ -7,51 +7,79 @@ import { AuthContext } from '../context/AuthContext';
 import { read } from '../apiCalls';
 
 const MyBooks = () => {
-  const myBooks =[] ;
-  //const [productsByArrival, setProductsByArrival] = useState([]);
-  const [error, setError] = useState([]);
+	const myBooks = [];
+	//const [productsByArrival, setProductsByArrival] = useState([]);
+	const [error, setError] = useState([]);
+	const { user } = useContext(AuthContext);
 
-  const loadProductsBySell = () => {
-    const userBooks = user.borrowedBooks;
-    for(let i=0; i<userBooks.length; i++){
-    read(userBooks[i]).then((data) => {
-         myBooks.push(data);
-      
-     });
-    }
-    console.log(myBooks);
-  };
+	const userBooks = user.borrowedBooks;
+	const loadProductsBySell = async () => {
+		for (let i = 0; i < userBooks.length; i++) {
+			await read(userBooks[i]).then((data) => {
+				myBooks.push(data);
+				// return data;
+			});
+		}
+		// console.log('1', myBooks);
+		// console.log('2', myBooks.length);
+	};
+	// console.log("userBooks", userBooks);
 
-  useEffect(() => {
-    loadProductsBySell();
-    //console.log(productsBySell);
-  }, []);
+	useEffect(() => {
+		loadProductsBySell();
+		// console.log(productsBySell);
+	});
 
-  const {user} = useContext(AuthContext);
+	const someFunction = () => {
+		userBooks.map((bookID, index) => {
+			read(userBooks[index])
+				.then((bookData) => {
+					const { name } = bookData;
+					return (
+						<div key={bookID}>
+							what the heck is wrong with you!!!!   print something
+							{console.log('book, index=', bookData, index)}
+							{console.log('book name=>', name)}
+							hii
+							fucking print something
+							<Card product={bookData} />
+							<h1>{name}</h1><br />
+						</div>
+					)
+				})
+		})
+	}
 
-  return (
-    <div>
-      <div className='row'>
-        <div className='col-md-1'></div>
-        <div className='col-md-10'>
-          
+	return (
+		<div>
+			the hell print somthing
+			<div className='row'>
+				<div className='col-md-1'></div>
+				<div className='col-md-10'>
 
-          <h2 className='mb-2 mt-4'>Borrowed Books</h2>
-          <div className='row'>
-            {myBooks.map((product, i) => (
-              <div key={i} className='col-xl-4 col-lg-6 col-md-6 col-sm-12'>
-              hii
-              <Card product={product} />
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className='col-md-1'></div>
-      </div>
-      </div>
-      
-    
-  );
+					<div className='mb-2 mt-4'>Borrowed Books</div>
+
+					<div className='row'>
+						{/* {console.log("myBooks", myBooks)} */}
+						{/* {myBooks.map((book) => {
+							const { id } = book;
+							console.log("book", book)
+							return (
+								<div key={id} className='col-xl-4 col-lg-6 col-md-6 col-sm-12'>
+									hii
+									<Card product={book} />
+								</div>
+							)
+						})} */}
+						{
+							someFunction()
+						}
+					</div>
+				</div>
+				<div className='col-md-1'></div>
+			</div>
+		</div >
+	);
 };
 
 export default MyBooks;
