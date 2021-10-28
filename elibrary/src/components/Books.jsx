d0415f03fd8fa88b11b57dd93fa59c7e9a51ffdc
vikash <vikashkumar754189@gmail.com>
2021-10-28 18:39:49 +0530

    cleaning the books.jsx

--- elibrary/src/components/Books.jsx
+++ elibrary/src/components/Books.jsx
@@ -1,24 +1,18 @@
-import React, { useState, useEffect } from 'react';
-//import Layout from './Layout';
-import { getProducts } from '../apiCalls';
-import Card from './Card';
-//import Search from './Search';
-//import 'fontsource-roboto';
-//import Copyright from './Copyright';
+import React, { useState, useEffect } from "react";
+
+import { getProducts } from "../apiCalls";
+import Card from "./Card";
 
 const Books = () => {
   const [productsBySell, setProductsBySell] = useState([]);
-  //const [productsByArrival, setProductsByArrival] = useState([]);
+
   const [error, setError] = useState([]);
 
   const loadProductsBySell = () => {
-    getProducts('rating').then((data) => {
-        setProductsBySell(data);
-      
+    getProducts("rating").then((data) => {
+      setProductsBySell(data);
     });
   };
-
- 
 
   useEffect(() => {
     loadProductsBySell();
@@ -27,25 +21,21 @@ const Books = () => {
 
   return (
     <div>
-      <div className='row'>
-        <div className='col-md-1'></div>
-        <div className='col-md-10'>
-          
-
-          <h2 className='mb-2 mt-4'></h2>
-          <div className='row'>
+      <div className="row">
+        <div className="col-md-1"></div>
+        <div className="col-md-10">
+          <h2 className="mb-2 mt-4"></h2>
+          <div className="row">
             {productsBySell.map((product, i) => (
-              <div key={i} className='col-xl-4 col-lg-6 col-md-6 col-sm-12'>
+              <div key={i} className="col-xl-4 col-lg-6 col-md-6 col-sm-12">
                 <Card product={product} />
               </div>
             ))}
           </div>
         </div>
-        <div className='col-md-1'></div>
+        <div className="col-md-1"></div>
       </div>
-      </div>
-      
-    
+    </div>
   );
 };
 
